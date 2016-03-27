e1cdd038de1aae16ae4e65b2df6c4df32d7a9630
Jason Dorje Short <jdorje@gmail.com>
2016-03-26 19:41:51 -0600

    * "Fix" a bunch of warnings. * Extract string resources.

--- app/src/main/java/com/dorjesoft/hwinforeader/Sensor.java
+++ app/src/main/java/com/dorjesoft/hwinforeader/Sensor.java
@@ -12,7 +12,7 @@ public class Sensor {
 
     // creates a sensor from a standard reader binary data
     Sensor(byte[] b, int offset) {
-        id = NetUtils.scanDwordInt(b, offset + 0);
+        id = NetUtils.scanDwordInt(b, offset);
         instance = (int) NetUtils.scanDwordInt(b, offset + 4);
         nameOrig = NetUtils.scanString(b, offset + 8, 128);
         nameUser = NetUtils.scanString(b, offset + 136, 128);
